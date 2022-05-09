e5c5ecfd7be0705d6fca9c1113642ac3c32bd67c
Pablo Sousa <pablossilva.005@gmail.com>
2022-05-09 18:43:42 -0300

    Add files via upload

---
+++ src/lista.c
@@ -0,0 +1,49 @@
+#include "lista.h"
+
+void FLVazia(Lista *l){
+    l->first = 0;
+    l->last  = 0;
+}
+
+void LInsert(Lista *l, Item d){
+    if (l->last >= MAXTAM){
+        printf("LISTA CHEIA!\n");
+    }else{
+        l->vet[l->last] = d;
+        l->last ++;
+    }
+}
+
+void LImprime(Lista *l){
+    for(int i=l->first; i<l->last; i++)
+        printf("%d\t", l->vet[i].val);
+    printf("\n");
+}
+
+void Verifica(Lista *l) {
+    int maxSum = -2147483648, cont = 0, soma = 0;
+    int vet[l->last];
+
+    for (int i = l->first; i < l->last; i++) {
+        for (int j = i; j < l->last; j++) {
+            soma += l->vet[j].val;
+            if (soma > maxSum) {
+                maxSum = soma;
+                vet[cont] = maxSum;
+            }
+        }
+        soma = 0;
+        maxSum = -2147483648;
+        cont++;
+    }
+
+    for (int i = l->first; i < l->last; i++) {
+        if (vet[i]>maxSum) {
+            maxSum = vet[i];
+        }
+    }
+
+    printf("\nA soma é %d\n", maxSum);
+
+
+}
---
+++ src/lista.h
@@ -0,0 +1,26 @@
+#ifndef LISTA_H 
+#define LISTA_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define MAXTAM 5
+
+typedef struct Item{
+    int val;
+} Item;
+
+typedef struct Lista{
+    Item vet[MAXTAM];
+    int first;
+    int last;
+} Lista;
+
+
+void FLVazia(Lista *l);
+void LInsert(Lista *l, Item d);
+void LImprime(Lista *l);
+void Verifica(Lista *l);
+
+#endif
